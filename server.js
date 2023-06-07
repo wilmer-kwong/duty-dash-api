72163b0e07910686c95244b4c363c63b6cf9735c
Wilmer Kwong <wkwong@litexindustries.com>
2023-06-07 14:23:07 -0500

    added cors middleware to all routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 
 // env config
 if (process.env.NODE_ENV !== 'production') {
@@ -19,6 +20,7 @@ mongoose.connect(dbUrl).then(() => {
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const PORT = 4000;
 
 app.use('/api', routes);
